7ecde1f7e85de93c757e096690839d5f8c8abef9
akshaykalucha3 <akshaykalucha@gmail.com>
2022-06-03 23:43:00 +0530

    initial commit

---
+++ Spektral/Main/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  Spektral
+//
+//  Created by Akshay Kalucha on 03/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
---
+++ Spektral/Main/SpektralApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpektralApp.swift
+//  Spektral
+//
+//  Created by Akshay Kalucha on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SpektralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
